eedb310445fb5d7cad81b8eb10e850bf1421e4cd
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-11 10:52:24 -0600

    Updates some formatting and unneeded enumerable calls

--- lib/library.rb
+++ lib/library.rb
@@ -15,13 +15,14 @@ class Library
   end
 
   def card_catalogue
-    @books.sort_by { |book| book.author_last_name }
+    # @books.sort_by { |book| book.author_last_name }
+    @books.sort_by(&:author_last_name)
   end
 
   def find_by_author(author_full_name)
     first_name = author_full_name.split[0]
     last_name = author_full_name.split[1]
-    result = @books.each.group_by do |book|
+    result = @books.group_by do |book|
       if book.author_first_name == first_name || book.author_last_name == last_name
         book.title
       end
