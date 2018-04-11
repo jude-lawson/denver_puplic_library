49818451201ff710226e4f3b834a2d389378ca5b
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-11 10:20:43 -0600

    Adds in test for adding multiple books to collection. Completes Iteration 3

--- test/library_test.rb
+++ test/library_test.rb
@@ -30,4 +30,30 @@ class LibraryTest < Minitest::Test
     assert_equal '10/16/1847', @dpl.books[0].publication_date
   end
 
+  def test_multiple_books_can_be_added
+    @dpl.add_to_collection(@jane_eyre)
+    @dpl.add_to_collection(@mockingbird)
+    @dpl.add_to_collection(@villette)
+
+    assert_instance_of Book, @dpl.books[0]
+    assert_equal 'Charlotte', @dpl.books[0].author_first_name
+    assert_equal 'Bronte', @dpl.books[0].author_last_name
+    assert_equal 'Jane Eyre', @dpl.books[0].title
+    assert_equal '10/16/1847', @dpl.books[0].publication_date
+
+    assert_instance_of Book, @dpl.books[1]
+    assert_equal 'Harper', @dpl.books[1].author_first_name
+    assert_equal 'Lee', @dpl.books[1].author_last_name
+    assert_equal 'To Kill a Mockingbird', @dpl.books[1].title
+    assert_equal '07/11/1960', @dpl.books[1].publication_date
+
+    assert_instance_of Book, @dpl.books[2]
+    assert_equal 'Charlotte', @dpl.books[2].author_first_name
+    assert_equal 'Bronte', @dpl.books[2].author_last_name
+    assert_equal 'Villette', @dpl.books[2].title
+    assert_equal '1853', @dpl.books[2].publication_date
+
+  end
+
+
 end
